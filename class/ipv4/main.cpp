278566cc7be94e07dbec291cedd80d236294c949
monero <forpetessake@protonmail.com>
2019-01-18 16:04:47 +0000

    add sorting code

--- class/ipv4/main.cpp
+++ class/ipv4/main.cpp
@@ -1,6 +1,9 @@
 #include "ipv4.hpp"
+#include "get_args.h"
 #include<iostream>
-int main()
+#include<algorithm>
+using namespace std;
+int main(int argc, char* argv[])
 {
    ipv4 a(168, 192, 0, 1);
    std::cout << a << std::endl;
@@ -15,4 +18,11 @@ int main()
    std::cin >> ip;
    if(!std::cin.fail())
       std::cout << ip << std::endl;
+   //test vector
+   cout << "getting the command line ip addresses" << endl;
+   vector<ipv4> ips = getCommandLineArgs<ipv4>(argc,argv);
+   sort(ips.begin(),ips.end());
+   for(auto &ip : ips) {
+      cout << ip << endl;
+   }   
 }
