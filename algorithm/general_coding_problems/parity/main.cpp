7c28b2693c938ecbfdf482d9ac02677f8fb1b191
monero <forpetessake@protonmail.com>
2019-01-23 18:58:56 +0000

    code for parity check updates

--- algorithm/general_coding_problems/parity/main.cpp
+++ algorithm/general_coding_problems/parity/main.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
+#include<random>
+#include<limits>
+#include<cassert>
+#include<algorithm>
+#include<string>
 #include "parity1.hpp"
 #include "parity2.hpp"
 #include "parity3.hpp"
+
+using std::default_random_engine;
+using std::numeric_limits;
+using std::random_device;
+using std::uniform_int_distribution;
+
+std::string toBinString(unsigned long long in ) {
+    std::string res="";
+    int count=0;
+    while(in) {
+        if (in & 1) {
+            res.append("1");
+            count++;
+        }
+        else res.append("0");
+        in >>= 1;
+    }
+    std::reverse(res.begin(),res.end());
+    std::cout << "num 1s=" << count << std::endl;
+    return res; 
+}
+
 using namespace std;
 int main(void ) {
     unsigned long long test1 = 0b1010;
@@ -13,6 +40,16 @@ int main(void ) {
     cout << Parity1::parity(test1) << endl;
     cout << Parity2::parity(test1) << endl;
     cout << Parity3::parity(test1) << endl;
+
+    default_random_engine gen((random_device())());
+    for(int i =0; i < 1000; i++) {
+        uniform_int_distribution<unsigned long long> dis(0, numeric_limits<unsigned long long>::max());
+        unsigned long long x = dis(gen);
+        assert(Parity1::parity(x) == Parity3::parity(x));
+        assert(Parity1::parity(x) == Parity2::parity(x));
+        cout << "x=" << x << ",parity = " << Parity3::parity(x) << endl;
+    }
   
+
     return 0;
 }
--- algorithm/general_coding_problems/parity/parity2.hpp
+++ algorithm/general_coding_problems/parity/parity2.hpp
@@ -1,9 +1,9 @@
 #pragma once
 namespace Parity2 {
-    bool parity(unsigned long long in) {
+    short parity(unsigned long long in) {
         short res = 0; // 1 is odd 0 is even
         while(in) {
-            res ^= (in & 1);
+            res ^= 1;
             in &= (in - 1);
         } 
         return res;
