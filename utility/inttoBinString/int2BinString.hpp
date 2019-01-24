f0764d46538054e0ba07ef218ff0426dc21eb90a
monero <forpetessake@protonmail.com>
2019-01-24 13:03:19 +0000

    remove leading zeroes

--- utility/inttoBinString/int2BinString.hpp
+++ utility/inttoBinString/int2BinString.hpp
@@ -14,7 +14,13 @@ std::string toBinString(unsigned long long in ) {
         else res.append("0");
         in >>= 1;
     }
+    //find first 1 to get to MSB 
+    size_t pos = res.find_first_of("1");
+    if (pos != std::string::npos) {
+
+        res = res.substr(pos);
+    }
     std::reverse(res.begin(),res.end());
-    std::cout << "num 1s=" << count << std::endl;
+    //std::cout << "num 1s=" << count << std::endl;
     return res; 
 }
