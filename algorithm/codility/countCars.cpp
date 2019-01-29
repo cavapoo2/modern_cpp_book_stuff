030c3017c618daf4b50065c2ba2ec935e3540927
monero <forpetessake@protonmail.com>
2019-01-29 13:17:49 +0000

    add moving cars count problem

---
+++ algorithm/codility/countCars.cpp
@@ -0,0 +1,76 @@
+/*
+ *A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+
+Array A contains only 0s and/or 1s:
+
+0 represents a car traveling east,
+1 represents a car traveling west.
+The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+
+For example, consider array A such that:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+
+Write a function:
+
+int solution(vector<int> &A);
+
+that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
+
+The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
+
+For example, given:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+the function should return 5, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer that can have one of the following values: 0, 1.
+ */ 
+template<typename Cont>
+void show(const Cont & c) {
+    for(const auto &v : c) {
+        cout << v << ","; 
+    }
+    cout << endl;
+} 
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    if(A.size() == 1) return 0;
+    vector<int> store;
+    int count=0;
+    for(int i=0; i < A.size(); ++i) {
+        if(A[i] == 0) {
+            if(store.size() > 0) {
+                store[store.size()-1] = count; 
+            }
+            count=0;
+            store.push_back(0);
+            
+        }else {
+            count++;
+        }
+    }
+    if(store.size() > 0)
+        store[store.size()-1] = count;
+    //now with store do the math
+   // show(store);
+    unsigned long long sumcount=0;
+    for(int i= store.size()-1; i >= 0; --i) {
+        sumcount = sumcount + store[i]*(i+1);     
+    }
+   
+    if (sumcount > 1000000000) return -1;
+    return static_cast<int>(sumcount);
+}
