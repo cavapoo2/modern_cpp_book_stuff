177c3e6baf280a63fa33414a5d4d95317893e3bf
monero <forpetessake@protonmail.com>
2019-02-02 19:36:34 +0000

    max prod of 3

---
+++ algorithm/codility/maxProdOf3.cpp
@@ -0,0 +1,55 @@
+/*
+ *A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+
+For example, array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+contains the following example triplets:
+
+(0, 1, 2), product is −3 * 1 * 2 = −6
+(1, 2, 4), product is 1 * 2 * 5 = 10
+(2, 4, 5), product is 2 * 5 * 6 = 60
+Your goal is to find the maximal product of any triplet.
+
+Write a function:
+
+int solution(vector<int> &A);
+
+that, given a non-empty array A, returns the value of the maximal product of any triplet.
+
+For example, given array A such that:
+
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [3..100,000];
+each element of array A is an integer within the range [−1,000..1,000].
+ */
+
+#include<algorithm>
+using namespace std;
+int solution(vector<int> &A) {
+    sort(A.begin(),A.end());
+    int from = A.size()-1;
+    int to = A.size()-3;
+    int sum=1;
+    for(int i = from; i >= to;--i) {
+        sum *= A[i];
+    }
+    //also try lower 2 and last in the case of 2 negatives
+    int other  = A[0]*A[1] *A[A.size()-1];
+    if (other > sum) return other;
+    else return sum;
+}
