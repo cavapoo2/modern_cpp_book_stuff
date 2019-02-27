916b27cc049b2cfdeb78e63de1ec7bb79a62aa2a
monero <forpetessake@protonmail.com>
2019-02-27 10:34:58 +0000

    update to skeleton main

--- skeleton/main.cpp
+++ skeleton/main.cpp
@@ -2,6 +2,17 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+using vi = vector<int>;
+
+
+template<typename Cont>
+void show(const Cont& c) {
+	for(const auto & v: c) {
+		cout << v << ",";
+	}
+	cout << endl;
+
+}
 
 int main(int arg, char* argv[])
 {
